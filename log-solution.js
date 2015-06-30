6d180242cac7108f8896c51cc92187e103f67dce
David Ernst <git@dsernst.com>
2015-06-30 10:08:33 -0700

    successfully sort ~20k out of ~30k logs

--- log-solution.js
+++ log-solution.js
@@ -1,5 +1,6 @@
 var Log = require("./Logs").Log;
 var Printer = require("./Logs").Printer;
+var Heap = require('heap');
 
 /**
  * Challenge Number 1!
@@ -23,62 +24,48 @@ var logs = [
     new Log()
   ];
 
-var Queue = require('data-structures').Queue;
-
-var mergeSortByProperty = function (array, propertyName) {
-  if (array.length === 1) {
-    return array.slice();
-  }
-
-  var split = Math.floor(array.length / 2);
-  var left = array.slice(0, split);
-  var right = array.slice(split);
-
-  left = mergeSortByProperty(left, propertyName);
-  right = mergeSortByProperty(right, propertyName);
-
-  var sorted = [];
-  while (left.length > 0 || right.length > 0) {
-    if (left.length === 0) {
-      sorted.push(right.shift());
-    } else if (right.length === 0 || left[0][propertyName] <= right[0][propertyName]) {
-      sorted.push(left.shift());
-    } else {
-      sorted.push(right.shift());
-    }
-  }
-
-  return sorted;
-};
-
 var syncSortedMerge = function (logs) {
+    // initially all log sources are unfinished
     var unfinishedLogSources = logs.slice();
-    var unsortedRecentLogs;
 
-    while (true) {
-      console.log('new loop \n');
-      unsortedRecentLogs = [];
-      // get the newest log from each of our unfinished log sources
-      unfinishedLogSources.forEach(function (log, index) {
-        if (log.done) {
-          // unfinishedLogSources.pop();
-          // this log source is all finished, splice it out
-          console.log('finished: ', index);
-          unfinishedLogSources.splice(index, 1);
-          return;
-        }
-        unsortedRecentLogs.push(log.pop());
-      });
+    // but we can check as we process them to filter out the ones that finish
+    function isUnfinished(log) {
+      return !log.done;
+    }
 
-      // console.log(unsortedRecentLogs);
-      mergeSortByProperty(unsortedRecentLogs, 'date').forEach(function (log) {
-        printer.print(log);
-      });
+    // this reduceable function will process incoming logs from the Log Sources
+    function grabMostRecentLogs(memo, log) {
+      memo.push(log.pop());
+      return memo;
+    }
+
+    // this heap will keep our logs min-sorted as we get them
+    var newestLogsHeap = new Heap(function (a, b) {
+      return a.date - b.date;
+    });
+
+    while (unfinishedLogSources.length > 0) {
+      // prune our finished log sources to tell our while loop knows when to finish
+      unfinishedLogSources = unfinishedLogSources.filter(isUnfinished);
+
+      // get the newest logs from each of our log sources
+      unfinishedLogSources.reduce(grabMostRecentLogs, newestLogsHeap);
+    }
+
+    // print out all of our sorted logs, with a little extra error handling
+    var numPrinted = 0;
+    try {
+      while (!newestLogsHeap.empty()) {
+        printer.print(newestLogsHeap.pop());
+        // newestLogsHeap.pop(); // to see how many total logs there are, uncomment this line and comment out the one above
+        numPrinted++;
+      }
+    } finally {
+      console.log('\nSuccessfully printed ', numPrinted, ' logs\n');
     }
   };
 
 syncSortedMerge(logs);
-// printer.print(logs);
 
 /**
  * Challenge Number 2!
