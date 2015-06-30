bb7f4943dd3d5e473ea4a49802aa70dcc5384fd6
David Ernst <git@dsernst.com>
2015-06-30 10:55:31 -0700

    successfully sort logs async

--- log-solution.js
+++ log-solution.js
@@ -1,6 +1,7 @@
 var Log = require("./Logs").Log;
 var Printer = require("./Logs").Printer;
 var Heap = require('heap');
+var Promise = require('bluebird');
 
 /**
  * Challenge Number 1!
@@ -45,7 +46,7 @@ var syncSortedMerge = function (logs) {
     });
 
     while (unfinishedLogSources.length > 0) {
-      // prune our finished log sources to tell our while loop knows when to finish
+      // prune our finished log sources to tell our while loop when to finish
       unfinishedLogSources = unfinishedLogSources.filter(isUnfinished);
 
       // get the newest logs from each of our log sources
@@ -61,11 +62,11 @@ var syncSortedMerge = function (logs) {
         numPrinted++;
       }
     } finally {
-      console.log('\nSuccessfully printed ', numPrinted, ' logs\n');
+      console.log('\nSuccessfully printed ', numPrinted, ' logs synchronously\n');
     }
   };
 
-syncSortedMerge(logs);
+// syncSortedMerge(logs);
 
 /**
  * Challenge Number 2!
@@ -87,7 +88,57 @@ var asyncLogs = [
   ];
 
 var asyncSortedMerge = function (logs) {
+    // this reduceable function will process incoming logs from the Log Sources
+    function grabMostRecentLogsAsync(memo, log) {
+      return new Promise(function (resolve, reject) {
+        log.popAsync(function (error, singleLog) {
+          if (error) {
+            reject(error);
+          }
+          memo.push(singleLog);
+          resolve(memo);
+        });
+      });
+    }
 
-};
+    // this heap will keep our logs min-sorted as we get them
+    var newestLogsHeap = new Heap(function (a, b) {
+      return a.date - b.date;
+    });
+
+    function getMostRecentLogs(logs) {
+      return new Promise(function (resolve) {
+        // prune out the log sources that are finished
+        var unfinishedLogSources = logs.filter(function isUnfinished(log) {
+          return !log.done;
+        });
+
+        // get the newest logs from each of our log sources
+        Promise.reduce(unfinishedLogSources, grabMostRecentLogsAsync, newestLogsHeap)
+          .then(function () {
+            resolve();
+          });
+
+        if (unfinishedLogSources.length > 0) {
+          getMostRecentLogs(unfinishedLogSources);
+        }
+      });
+    }
+
+    getMostRecentLogs(logs)
+      .then(function () {
+        // print out all of our sorted logs, with a little extra error handling
+        var numPrinted = 0;
+        try {
+          while (!newestLogsHeap.empty()) {
+            printer.print(newestLogsHeap.pop());
+            // newestLogsHeap.pop(); // to see how many total logs there are, uncomment this line and comment out the one above
+            numPrinted++;
+          }
+        } finally {
+          console.log('\nSuccessfully printed ', numPrinted, ' logs asynchronously\n');
+        }
+      });
+  };
 
 asyncSortedMerge(asyncLogs);
