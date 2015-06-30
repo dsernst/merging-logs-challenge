9b920409a1f13ac6366a1e8e82a5600d37778677
David Ernst <git@dsernst.com>
2015-06-30 11:20:42 -0700

    adjust indentation (no functional changes)

--- log-solution.js
+++ log-solution.js
@@ -26,45 +26,45 @@ var logs = [
   ];
 
 var syncSortedMerge = function (logs) {
-    // initially all log sources are unfinished
-    var unfinishedLogSources = logs.slice();
+  // initially all log sources are unfinished
+  var unfinishedLogSources = logs.slice();
 
-    // but we can check as we process them to filter out the ones that finish
-    function isUnfinished(log) {
-      return !log.done;
+  // but we can check as we process them to filter out the ones that finish
+  function isUnfinished(log) {
+    return !log.done;
+  }
+
+  // this reduceable function will process incoming logs from the Log Sources
+  function grabMostRecentLogs(memo, log) {
+    memo.push(log.pop());
+    return memo;
+  }
+
+  // this heap will keep our logs min-sorted as we get them
+  var newestLogsHeap = new Heap(function (a, b) {
+    return a.date - b.date;
+  });
+
+  while (unfinishedLogSources.length > 0) {
+    // prune our finished log sources to tell our while loop when to finish
+    unfinishedLogSources = unfinishedLogSources.filter(isUnfinished);
+
+    // get the newest logs from each of our log sources
+    unfinishedLogSources.reduce(grabMostRecentLogs, newestLogsHeap);
+  }
+
+  // print out all of our sorted logs, with a little extra error handling
+  var numPrinted = 0;
+  try {
+    while (!newestLogsHeap.empty()) {
+      printer.print(newestLogsHeap.pop());
+      // newestLogsHeap.pop(); // to see how many total logs there are, uncomment this line and comment out the one above
+      numPrinted++;
     }
-
-    // this reduceable function will process incoming logs from the Log Sources
-    function grabMostRecentLogs(memo, log) {
-      memo.push(log.pop());
-      return memo;
-    }
-
-    // this heap will keep our logs min-sorted as we get them
-    var newestLogsHeap = new Heap(function (a, b) {
-      return a.date - b.date;
-    });
-
-    while (unfinishedLogSources.length > 0) {
-      // prune our finished log sources to tell our while loop when to finish
-      unfinishedLogSources = unfinishedLogSources.filter(isUnfinished);
-
-      // get the newest logs from each of our log sources
-      unfinishedLogSources.reduce(grabMostRecentLogs, newestLogsHeap);
-    }
-
-    // print out all of our sorted logs, with a little extra error handling
-    var numPrinted = 0;
-    try {
-      while (!newestLogsHeap.empty()) {
-        printer.print(newestLogsHeap.pop());
-        // newestLogsHeap.pop(); // to see how many total logs there are, uncomment this line and comment out the one above
-        numPrinted++;
-      }
-    } finally {
-      console.log('\nSuccessfully printed ', numPrinted, ' logs synchronously\n');
-    }
-  };
+  } finally {
+    console.log('\nSuccessfully printed ', numPrinted, ' logs synchronously\n');
+  }
+};
 
 // syncSortedMerge(logs);
 
@@ -88,57 +88,57 @@ var asyncLogs = [
   ];
 
 var asyncSortedMerge = function (logs) {
-    // this promisified reduceable function will process incoming logs from the log sources
-    function grabMostRecentLogsAsync(memo, log) {
-      return new Promise(function (resolve, reject) {
-        log.popAsync(function (error, singleLog) {
-          if (error) {
-            reject(error);
-          }
-          memo.push(singleLog);
-          resolve(memo);
-        });
+  // this promisified reduceable function will process incoming logs from the log sources
+  function grabMostRecentLogsAsync(memo, log) {
+    return new Promise(function (resolve, reject) {
+      log.popAsync(function (error, singleLog) {
+        if (error) {
+          reject(error);
+        }
+        memo.push(singleLog);
+        resolve(memo);
       });
-    }
-
-    // this heap will keep our logs min-sorted as we get them
-    var newestLogsHeap = new Heap(function (a, b) {
-      return a.date - b.date;
     });
+  }
 
-    function getMostRecentLogs(logs) {
-      return new Promise(function (resolve) {
-        // prune out the log sources that finished
-        var unfinishedLogSources = logs.filter(function isUnfinished(log) {
-          return !log.done;
+  // this heap will keep our logs min-sorted as we get them
+  var newestLogsHeap = new Heap(function (a, b) {
+    return a.date - b.date;
+  });
+
+  function getMostRecentLogs(logs) {
+    return new Promise(function (resolve) {
+      // prune out the log sources that finished
+      var unfinishedLogSources = logs.filter(function isUnfinished(log) {
+        return !log.done;
+      });
+
+      // get the newest logs from each of our log sources
+      Promise.reduce(unfinishedLogSources, grabMostRecentLogsAsync, newestLogsHeap)
+        .then(function () {
+          resolve();
         });
 
-        // get the newest logs from each of our log sources
-        Promise.reduce(unfinishedLogSources, grabMostRecentLogsAsync, newestLogsHeap)
-          .then(function () {
-            resolve();
-          });
+      if (unfinishedLogSources.length > 0) {
+        getMostRecentLogs(unfinishedLogSources);
+      }
+    });
+  }
 
-        if (unfinishedLogSources.length > 0) {
-          getMostRecentLogs(unfinishedLogSources);
+  getMostRecentLogs(logs)
+    .then(function () {
+      // print out all of our sorted logs, with a little extra error handling
+      var numPrinted = 0;
+      try {
+        while (!newestLogsHeap.empty()) {
+          printer.print(newestLogsHeap.pop());
+          // newestLogsHeap.pop(); // to see how many total logs there are, uncomment this line and comment out the one above
+          numPrinted++;
         }
-      });
-    }
-
-    getMostRecentLogs(logs)
-      .then(function () {
-        // print out all of our sorted logs, with a little extra error handling
-        var numPrinted = 0;
-        try {
-          while (!newestLogsHeap.empty()) {
-            printer.print(newestLogsHeap.pop());
-            // newestLogsHeap.pop(); // to see how many total logs there are, uncomment this line and comment out the one above
-            numPrinted++;
-          }
-        } finally {
-          console.log('\nSuccessfully printed ', numPrinted, ' logs asynchronously\n');
-        }
-      });
-  };
+      } finally {
+        console.log('\nSuccessfully printed ', numPrinted, ' logs asynchronously\n');
+      }
+    });
+};
 
 asyncSortedMerge(asyncLogs);
