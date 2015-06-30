f4cd2d021fa6c11bc5c0a85ffff1d565fc747e96
David Ernst <git@dsernst.com>
2015-06-30 09:11:06 -0700

    mergeSort attempt on sync

--- log-solution.js
+++ log-solution.js
@@ -1,5 +1,5 @@
-var Log = require("./Logs").Log,
-    Printer = require("./Logs").Printer;
+var Log = require("./Logs").Log;
+var Printer = require("./Logs").Printer;
 
 /**
  * Challenge Number 1!
@@ -21,13 +21,64 @@ var logs = [
     new Log(),
     new Log(),
     new Log()
-];
+  ];
 
-var syncSortedMerge = function (logs) {
+var Queue = require('data-structures').Queue;
 
+var mergeSortByProperty = function (array, propertyName) {
+  if (array.length === 1) {
+    return array.slice();
+  }
+
+  var split = Math.floor(array.length / 2);
+  var left = array.slice(0, split);
+  var right = array.slice(split);
+
+  left = mergeSortByProperty(left, propertyName);
+  right = mergeSortByProperty(right, propertyName);
+
+  var sorted = [];
+  while (left.length > 0 || right.length > 0) {
+    if (left.length === 0) {
+      sorted.push(right.shift());
+    } else if (right.length === 0 || left[0][propertyName] <= right[0][propertyName]) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+
+  return sorted;
 };
 
+var syncSortedMerge = function (logs) {
+    var unfinishedLogSources = logs.slice();
+    var unsortedRecentLogs;
+
+    while (true) {
+      console.log('new loop \n');
+      unsortedRecentLogs = [];
+      // get the newest log from each of our unfinished log sources
+      unfinishedLogSources.forEach(function (log, index) {
+        if (log.done) {
+          // unfinishedLogSources.pop();
+          // this log source is all finished, splice it out
+          console.log('finished: ', index);
+          unfinishedLogSources.splice(index, 1);
+          return;
+        }
+        unsortedRecentLogs.push(log.pop());
+      });
+
+      // console.log(unsortedRecentLogs);
+      mergeSortByProperty(unsortedRecentLogs, 'date').forEach(function (log) {
+        printer.print(log);
+      });
+    }
+  };
+
 syncSortedMerge(logs);
+// printer.print(logs);
 
 /**
  * Challenge Number 2!
@@ -46,7 +97,7 @@ var asyncLogs = [
     new Log(),
     new Log(),
     new Log()
-];
+  ];
 
 var asyncSortedMerge = function (logs) {
 
